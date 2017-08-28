8b7028964ec7ec9520520647d4b048fde682b42e
Jusiel Tunu <jusiel.tunu@gmail.com>
2017-08-28 18:07:38 -0300

    giovanni

---
+++ PushNotification4/customActivity.js
@@ -0,0 +1,100 @@
+define([
+    'postmonger'
+], function(
+    Postmonger
+) {
+    'use strict';
+
+    var connection = new Postmonger.Session();
+    var payload = {};
+    var lastStepEnabled = false;
+    var steps = [ // initialize to the same value as what's set in config.json for consistency
+        { "label": "Step 1", "key": "step1" }
+    ];
+    var currentStep = steps[0].key;
+
+    $(window).ready(onRender);
+
+    connection.on('initActivity', initialize);
+    connection.on('requestedTokens', onGetTokens);
+    connection.on('requestedEndpoints', onGetEndpoints);
+
+    connection.on('clickedNext', onClickedNext);
+    connection.on('clickedBack', onClickedBack);
+    connection.on('gotoStep', onGotoStep);
+
+    function onRender() {
+        connection.trigger('ready');
+    }
+
+    function initialize (data) {
+        if (data) {
+            payload = data;
+        }
+    }
+
+    function onGetTokens (tokens) {
+        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+        // console.log(tokens);
+    }
+
+    function onGetEndpoints (endpoints) {
+        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+        // console.log(endpoints);
+    }
+
+    function onClickedNext () {
+      if (currentStep.key === 'step1') {
+            save();
+        } else {
+            connection.trigger('nextStep');
+        }
+    }
+
+    function onClickedBack () {
+        connection.trigger('prevStep');
+    }
+
+    function onGotoStep (step) {
+        showStep(step);
+        connection.trigger('ready');
+    }
+
+    function showStep(step, stepIndex) {
+        if (stepIndex && !step) {
+            step = steps[stepIndex - 1];
+        }
+
+        currentStep = step;
+
+        $('.step').hide();
+
+        switch  (currentStep.key) {
+        case 'eventdefinitionkey':
+            $('#step1').show();
+            $('#step1 input').focus();
+            break;
+        }
+    }
+
+    function save() {
+       var eventDefinitionKey = $('#select-entryevent-defkey').val();
+
+        // 'payload' is initialized on 'initActivity' above.
+        // Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property
+        // may be overridden as desired.
+        payload.name = name;
+
+        payload['arguments'].execute.inArguments = [{ "message": eventDefinitionKey }];
+
+        payload['metaData'].isConfigured = true;
+
+        connection.trigger('updateActivity', payload);
+    }
+
+    // function getMessage() {
+    //     return $('#select1').find('option:selected').attr('value').trim();
+    // }
+
+});
