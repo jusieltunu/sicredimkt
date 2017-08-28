256dda3772f5a37d058f01a5b59ec560a8493311
Giovanni Prudencio <prudencio.giovanni@gmail.com>
2017-08-28 18:52:21 -0300

    refactor

--- PushNotification4/customActivity.js
+++ PushNotification4/customActivity.js
@@ -9,7 +9,8 @@ define([
     var payload = {};
     var lastStepEnabled = false;
     var steps = [ // initialize to the same value as what's set in config.json for consistency
-        { "label": "Step 1", "key": "step1" }
+        { "label": "Step 1", "key": "step1" },
+        { "label": "Step 2", "key": "step2" }
     ];
     var currentStep = steps[0].key;
 
@@ -24,12 +25,62 @@ define([
     connection.on('gotoStep', onGotoStep);
 
     function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
+
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+
+        // Disable the next button if a value isn't selected
+        $('#inputMessage').change(function() {
+            var message = getMessage();
+            connection.trigger('updateButton', { button: 'next', enabled: Boolean(message) });
+
+            $('#message').html(message);
+        });
+
+        // Toggle step 4 active/inactive
+        // If inactive, wizard hides it and skips over it during navigation
+        $('#toggleLastStep').click(function() {
+            lastStepEnabled = !lastStepEnabled; // toggle status
+            steps[3].active = !steps[3].active; // toggle active
+
+            connection.trigger('updateSteps', steps);
+        });
     }
 
     function initialize (data) {
         if (data) {
             payload = data;
+        }
+
+        var message;
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+
+        $.each(inArguments, function(index, inArgument) {
+            $.each(inArgument, function(key, val) {
+                if (key === 'message') {
+                    message = val;
+                }
+            });
+        });
+
+        // If there is no message selected, disable the next button
+        if (!message) {
+            showStep(null, 1);
+            connection.trigger('updateButton', { button: 'next', enabled: false });
+            // If there is a message, skip to the summary step
+        } else {
+            $('#inputMessage').val(message);
+            $('#message').html(message);
+            showStep(null, 3);
         }
     }
 
@@ -44,7 +95,10 @@ define([
     }
 
     function onClickedNext () {
-      if (currentStep.key === 'step1') {
+        if (
+            (currentStep.key === 'step3' && steps[3].active === false) ||
+            currentStep.key === 'step4'
+        ) {
             save();
         } else {
             connection.trigger('nextStep');
@@ -62,23 +116,66 @@ define([
 
     function showStep(step, stepIndex) {
         if (stepIndex && !step) {
-            step = steps[stepIndex - 1];
+            step = steps[stepIndex-1];
         }
 
         currentStep = step;
 
         $('.step').hide();
 
-        switch  (currentStep.key) {
+        switch(currentStep.key) {
             case 'step1':
                 $('#step1').show();
-                $('#step1 input').focus();
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    enabled: Boolean(getMessage())
+                });
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: false
+                });
+                break;
+            case 'step2':
+                $('#step2').show();
+                connection.trigger('updateButton', {
+                    button: 'back',
+                    visible: true
+                });
+                connection.trigger('updateButton', {
+                    button: 'next',
+                    text: 'next',
+                    visible: true
+                });
+                break;
+            case 'step3':
+                $('#step3').show();
+                connection.trigger('updateButton', {
+                     button: 'back',
+                     visible: true
+                });
+                if (lastStepEnabled) {
+                    connection.trigger('updateButton', {
+                        button: 'next',
+                        text: 'next',
+                        visible: true
+                    });
+                } else {
+                    connection.trigger('updateButton', {
+                        button: 'next',
+                        text: 'done',
+                        visible: true
+                    });
+                }
+                break;
+            case 'step4':
+                $('#step4').show();
                 break;
         }
     }
 
     function save() {
-       var eventDefinitionKey = $('#select-entryevent-defkey').val();
+        var name = $('#inputMessage').val().html();
+        var value = getMessage();
 
         // 'payload' is initialized on 'initActivity' above.
         // Journey Builder sends an initial payload with defaults
@@ -86,7 +183,7 @@ define([
         // may be overridden as desired.
         payload.name = name;
 
-        payload['arguments'].execute.inArguments = [{ "message": eventDefinitionKey }];
+        payload['arguments'].execute.inArguments = [{ "message": value }];
 
         payload['metaData'].isConfigured = true;
 
@@ -94,7 +191,7 @@ define([
     }
 
     function getMessage() {
-        return $('#select-entryevent-defkey').val().trim();
+        return $('#inputMessage').val().trim();
     }
 
 });
