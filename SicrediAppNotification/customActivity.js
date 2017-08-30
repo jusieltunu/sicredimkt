81f2054aab5eec0a156ec138fd9199cab08e3351
Jusiel Tunu <jusiel.tunu@gmail.com>
2017-08-30 19:05:06 -0300

    commir

--- SicrediAppNotification/customActivity.js
+++ SicrediAppNotification/customActivity.js
@@ -165,7 +165,7 @@ define([
         connection.trigger('updateActivity', payload);
     }
 
-    function camposPreenchidos(){
+    function  camposPreenchidos(){
         if($('#action').val() == '' || $('#title').val() == '' || $('#from').val() == '' || $('#message').val() == ''){
             return false;
         }
